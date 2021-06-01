dd4a1083e18956103280cc99312766f04817b731
nononon <fuck@you.com>
2021-06-02 00:30:44 +0700

    comments on 7004-lm-v3

--- src/common/nvp6134/ad_cfg.h
+++ src/common/nvp6134/ad_cfg.h
@@ -3,6 +3,18 @@
 
 #include "sample_comm.h"
 
+/**
+ * Вобщем закоменчу сюда выводы по 7004-lm-v3.
+ * Плата имеет hi3520d проц, nvp6134 adc конвертер.
+ * на openipc патченная софия грузит проц и лагает стрим иногда, но в целом работает.
+ * 1080n = 1080p Width/2 что плохо
+ * nvp умеет 1080p не в mix режиме, а вот hi3520d умеет только на dev0->ch0
+ * при этом все другие каналы и dev1 не юзабельные.
+ * Инфа по hi3520d ограничениям в HiMPP Media Processing Software Development Reference.pdf стр. 121.
+ * Инфа по nvp6134 ограничении в mix режиме NVP6134C_Datasheet_release_v01_161012.pdf стр. 25
+ */
+
+
 void adInit();
 
 struct ChannelInfo {
