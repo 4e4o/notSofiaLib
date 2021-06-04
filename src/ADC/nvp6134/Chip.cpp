24ee5fdcabfb27a68a5e5a09053625dec3f020a1
nononon <fuck@you.com>
2021-06-04 17:28:21 +0700

    nvp chip init moved to configure stage

--- src/ADC/nvp6134/Chip.cpp
+++ src/ADC/nvp6134/Chip.cpp
@@ -10,18 +10,9 @@ namespace nvp6134 {
 Chip::Chip(DriverCommunicator* d, int id)
     : IdHolder(id),
       m_driver(d) {
-    init();
 }
 
 Chip::~Chip() {
-}
-
-void Chip::init() {
-    for (int i = 0 ; i < (int) m_viChannels.size() ; i++)
-        m_viChannels[i].reset(new ViChannel(this, i));
-
-    for (int i = 0 ; i < (int) m_voChannels.size() ; i++)
-        m_voChannels[i].reset(new VoChannel(this, i));
 }
 
 DriverCommunicator* Chip::driver() const {
@@ -52,6 +43,13 @@ NVP6134_OUTMODE_SEL Chip::getVoChannelMode(VoChannel*) {
 }
 
 bool Chip::configure() {
+    // создаём каналы
+    for (int i = 0 ; i < (int) m_viChannels.size() ; i++)
+        m_viChannels[i].reset(new ViChannel(this, i));
+
+    for (int i = 0 ; i < (int) m_voChannels.size() ; i++)
+        m_voChannels[i].reset(new VoChannel(this, i));
+
     // Устанавливаем режим Vi каналов
     for (int i = 0 ; i < (int) m_viChannels.size() ; i++) {
         m_viChannels[i]->setMode(getViChannelMode(m_viChannels[i].get()));
